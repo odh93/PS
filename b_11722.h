2462a592fc4b71984de1c5a25d42112b6ce06048
odh93 <38158355+odh93@users.noreply.github.com>
2020-05-07 16:04:07 +0900

    Update b_11722.h

--- b_11722.h
+++ b_11722.h
@@ -16,15 +16,19 @@ int function()
 	for (int i = 0; i < n; i++)
 		cin >> arr[i];
 	ans = 1;
+	/* 문제에서 답으로 요구하는 것은 가장 긴 감소하는 부분 수열의 길이이다.
+	 따라서 dp배열에 index가 왼쪽부터 시작해서 오른쪽으로 가는 방향으로 
+	 각 index위치에서의 나올 수 있는 부분 수열의 가장 긴 감소하는 길이 값을 저장해주는 
+	 메모이제이션 방식을 이용해서 문제를 해결하면 된다.*/
 	for (int i = 0; i < n; i++)
 	{
-		dp[i] = 1;
-		for (int j = 0; j < i; j++)
+		dp[i] = 1; // 각 위치의 시작 길이는 1로 한다.
+		for (int j = 0; j < i; j++) // 현재 index보다 왼쪽의 index들을 차례로 비교
 		{
-			if (arr[j] > arr[i] && dp[i] < dp[j] + 1)
+			if (arr[j] > arr[i] && dp[i] < dp[j] + 1) // 현재보다 배열값이 작고 비교하는 위치의 길이+1보다 현재 위치가 작으면 갱신
 				dp[i] = dp[j] + 1;
 		}
-		ans = max(ans, dp[i]);
+		ans = max(ans, dp[i]); //  매위치 계산이 끝난 최대길이와 정답 값을 비교해서 갱신
 	}
 	cout << ans;
 	return 0;
