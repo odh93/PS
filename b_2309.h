3c36840ccc24394d87343e816de2137ec6b7decb
odh93 <38158355+odh93@users.noreply.github.com>
2020-05-07 17:50:53 +0900

    Update b_2309.h

--- b_2309.h
+++ b_2309.h
@@ -1,31 +1,38 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
-
-int n, k;
-long long dp[201][201],mod = 1000000000;
+int arr[9],sum;
 int function()
 {
 	freopen("input.txt", "r", stdin);
 	setbuf(stdout, NULL);
-
 	ios_base::sync_with_stdio(false);
 	cin.tie(0), cout.tie(0);
-
-	cin >> n >> k;
-
-	for (int i = 0; i <= n; i++)
-		dp[1][i] = 1 % mod;
-	for (int a = 2; a <= k; a++)
+	for (int i = 0; i < 9; i++)
 	{
-		for (int i = 0; i <= n; i++)
+		cin >> arr[i];
+		sum += arr[i];
+	}
+	sort(arr, arr + 9);
+	// 오름차순으로 출력하기에 정렬해준다.
+
+	/* 해결방법은 7명중 합이 100되는 5명을 찾는 과정이니 포함안되는 2명을 찾는것이 빠르다.
+	재귀형식을 짤 필요 없이 for문으로 빠르게 구해주었다.*/
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = i + 1; j < 9; j++)
 		{
-			for (int j = 0; j <= i; j++)
+			if (sum - arr[i] - arr[j] == 100)
 			{
-				dp[a][i] += dp[a - 1][j] % mod;
+				for (int k = 0; k < 9; k++)
+				{
+					if (k == i || k == j)
+						continue;
+					cout << arr[k] << '\n';
+				}
+				exit(0);
 			}
 		}
 	}
-	cout << dp[k][n] % mod;
 	return 0;
 }
