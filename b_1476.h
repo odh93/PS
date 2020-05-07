d43a5851fdcae033fe4707e0fec2f879ac284659
odh93 <38158355+odh93@users.noreply.github.com>
2020-05-07 17:53:51 +0900

    Update b_1476.h

--- b_1476.h
+++ b_1476.h
@@ -15,22 +15,22 @@ int function()
 	cin >> e >> s >> m;
 
 	int te, ts, tm;
-	for (int i = 1; ; i++)
+	for (int i = 1; ; i++) // 정답은 항상있으니 무한for문으로 검사
 	{
-		if (i % 15 == 0)
+		if (i % 15 == 0) // 나머지가 0일시 E는 15
 			te = 15;
 		else
 			te = i % 15;
-		if (i % 28 == 0)
+		if (i % 28 == 0) // 나머지가 0일시 S는 28
 			ts = 28;
 		else
 			ts = i % 28;
-		if (i % 19 == 0)
+		if (i % 19 == 0) // 나머지가 0일시 M은 19
 			tm = 19;
 		else
 			tm= i % 19;
 		
-		if (e == te && s == ts && m == tm)
+		if (e == te && s == ts && m == tm) // 모두 기준과 같아지면 정답 출력
 		{
 			cout << i;
 			break;
