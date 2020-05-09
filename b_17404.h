47a86c50983acf824b5898ce5a297be94bc4a4bb
odh93 <38158355+odh93@users.noreply.github.com>
2020-05-09 10:27:44 +0900

    Update b_17404.h

--- b_17404.h
+++ b_17404.h
@@ -1,37 +1,45 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
-
-int n, k;
-long long dp[201][201],mod = 1000000000;
+int ans = 987654321,n,arr[1001][3],dp[1001][3];
 int function()
 {
 	freopen("input.txt", "r", stdin);
 	setbuf(stdout, NULL);
-
 	ios_base::sync_with_stdio(false);
 	cin.tie(0), cout.tie(0);
-
-	cin >> n >> k;
-
-	for (int i = 0; i <= n; i++)
-		dp[1][i] = 1 % mod;
-
-	/* dp[k][n] =k가 선택해서 합이 n이 되는 경우의수로 botoom-up방식으로 진행할
-	dp배열을 만들어 준다.
-	dp를 갱신하는 방법은 선택한 수의 숫자로 bottom-up을 진행해주는데 
-	 dp[a][i] , a가 선택해서 i를 만들 수 있는 경우의수는 a-1개 선택한 0부터 i까지의 dp배열을 더해주는 것이다.
-	 i이하의 수는 어떤 수이든 하나의 수를 더해서 i를 만들 수 있기 때문이다.*/
-	for (int a = 2; a <= k; a++)
+	cin >> n;
+	for (int i = 1; i <= n; i++)
 	{
-		for (int i = 0; i <= n; i++) // 만들고 싶은 수
+		for (int j = 0; j < 3; j++)
+			cin >> arr[i][j];
+	}
+	/* 1번집과 n번집의 색깔이 달라야 하는 것에 초점을 둬서
+	첫번째집의 색을 정하고 그것을 기준으로 dp를 따로 세워준다*/
+	for (int i = 0; i < 3; i++) // 첫번째집의 색을 정하는 for문
+	{
+		for (int j = 0; j < 3; j++) // 그 색이 일치하면 값을 주고 아니면 선택할 수 없게 최대값이상의 값을준다.
 		{
-			for (int j = 0; j <= i; j++) // 그 수이하의 수의 dp를 다 더해준다.
-			{
-				dp[a][i] += dp[a - 1][j] % mod;
-			}
+			if (i == j)
+				dp[1][j] = arr[1][j];
+			else
+				dp[1][j] = 987654321;
+		}
+
+		for (int j = 2; j <= n; j++) // 순서대로 빨강,초록,파랑을 0,1,2배열에
+		{
+			dp[j][0] = min(dp[j - 1][1], dp[j - 1][2]) + arr[j][0];
+			dp[j][1] = min(dp[j - 1][0], dp[j - 1][2]) + arr[j][1];
+			dp[j][2] = min(dp[j - 1][0], dp[j - 1][1]) + arr[j][2];
+		}
+
+		for (int j = 0; j < 3; j++)
+		{
+			if (i == j) // 마지막배열은 첫번째집의 색과 다른색들만 갱신해준다.
+				continue;
+			ans = min(ans, dp[n][j]);
 		}
 	}
-	cout << dp[k][n] % mod;
+	cout << ans;
 	return 0;
 }
